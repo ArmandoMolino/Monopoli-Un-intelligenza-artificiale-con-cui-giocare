d2c6dd4901286799dc4fad9277fe35626a5dd97b
ArmandoMolino <60621906+ArmandoMolino@users.noreply.github.com>
2022-06-10 11:02:01 +0200

    Update Stat.py

--- MonopoliRL/Stat.py
+++ MonopoliRL/Stat.py
@@ -12,8 +12,9 @@ class Stat(object):
         self._episode_rewards = np.zeros(num_episodes)
         self._win_rate = np.zeros(2)
         self._winRateLabel = ["Player 0(red)", "Player 1(blue)"]
-        self._win_block = np.zeros(40)
-        self._blockLabel = ["Via", 'Vicolo\nCorto', 'Probabilità\n1', 'Vicolo\nStretto', "Tassa\nPatrimoniale\n1",
+        self._win_block = np.zeros(28)
+        """
+        ["Via", 'Vicolo\nCorto', 'Probabilità\n1', 'Vicolo\nStretto', "Tassa\nPatrimoniale\n1",
                             "Stazione\nSud", "Bastioni\nGran\nSasso", "Imprevisti\n1", "Viale\nMonterosa",
                             "Viale\nVesuvio", "Transito", "Via\nAccademia", "Società\nElettrica", "Corso\nAteneo",
                             "Piazza\nUniversità", "Stazione\nOvest", "Via\nVerdi", "Probabilità\n2", "Corso\nRaffaello",
@@ -22,6 +23,13 @@ class Stat(object):
                             "Piazza\nGiulio\nCesare", "In\nprigione", "Via\nRoma", "Corso\nImpero", "Probabilità\n3",
                             "Largo\nAugusto", "Stazione\nEst", "Imprevisti\n3", "Viale\ndei\nGiardini",
                             "Tassa\nPatrimoniale\n2", "Parco\ndella\nVittoria", "Prigione"]
+        """
+        self._blockLabel = ['Vicolo\nCorto', 'Vicolo\nStretto', "Stazione\nSud", "Bastioni\nGran\nSasso",
+                            "Viale\nMonterosa", "Viale\nVesuvio", "Via\nAccademia", "Società\nElettrica",
+                            "Corso\nAteneo", "Piazza\nUniversità", "Stazione\nOvest", "Via\nVerdi", "Corso\nRaffaello",
+                            "Piazza\nDante", "Via\nMarco\nPolo", "Corso\nMagellano", "Largo\nColombo", "Stazione\nNord",
+                            "Viale\nCostantino", "Viale\nTraiano", "Fontane", "Piazza\nGiulio\nCesare", "Via\nRoma",
+                            "Corso\nImpero", "Largo\nAugusto", "Stazione\nEst", "Viale\ndei\nGiardini", "Parco\ndella\nVittoria"]
         self.label = label
 
     def __str__(self):
@@ -74,8 +82,11 @@ class Stat(object):
         self._win_rate[id] += 1
 
     def set_win_block(self, ids):
+        parserID = {1: 0, 3: 1, 5: 2, 6: 3, 8: 4, 9: 5, 11: 6, 12: 7, 13: 8, 14: 9, 15: 10, 16: 11, 18: 12, 19: 13,
+                    21: 14, 23: 15, 24: 16, 25: 17, 26: 18, 27: 19, 28: 20, 29: 21, 31: 22, 32: 23, 34: 24, 35: 25,
+                    37: 26, 39: 27}
         for id in ids:
-            self._win_block[id] += 1
+            self._win_block[parserID[id]] += 1
 
     @classmethod
     def plot_stats(cls, stats, width=0.85, noshow=False, title_font=None, ax_font=None, smoothing_window=100):
@@ -122,7 +133,7 @@ class Stat(object):
         c = width/2
         bar = []
         for i, k in enumerate(d):
-            x = np.arange(0, len(k)*np.ceil(len(d)/2), np.ceil(len(d)/2))
+            x = np.arange(0, len(k) * np.ceil(len(d)/2), np.ceil(len(d)/2))
             if len(d) % 2 == 0:
                 bar.append(ax.bar(x + c + (i - len(d)/2) * width, k, width))
             else:
